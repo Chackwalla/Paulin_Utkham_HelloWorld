e42c88b6ea2343e36effed261f13c2738ff68e93
Chackwalla <70134856+Chackwalla@users.noreply.github.com>
2021-08-27 13:12:03 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! / sx");
+            Console.WriteLine("I'm in pain");
+
+            //INT
+
+            int age = 18;
+            string name = "Payne";
+            double moooooonies = 130.78;
+
+            Console.WriteLine("The name is {0} and my age is {1}. I'm pretty broke, only have {2} on my account", name, age, moooooonies);
+           // Console.WriteLine("");
+           // Console.WriteLine(moooooonies);
+
+
+
+        }
+    }
+}
